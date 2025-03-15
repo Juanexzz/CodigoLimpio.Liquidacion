7251188efbf3814c8bfba915c008323bd83cdeaa
Emblask <salas.mangel96@gmail.com>
2025-03-14 21:48:47 -0400

    resolved other issues

--- src/view/console/main.py
+++ src/view/console/main.py
@@ -1,4 +1,10 @@
-import liquidacion_total
+import sys
+import os
+
+# Agrega la carpeta 'src' al path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../..")))
+
+import model.liquidacion_total
 
 try:
     salario_auxilio = float(input("Ingrese el salario con auxilio: "))
