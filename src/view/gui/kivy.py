e81fa18670743b69a2bc83e8d523cadcf26f85d1
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-04-02 13:36:56 -0500

    creacion del archivo kivy_liquidador y creacion del primer metodo build

---
+++ src/view/gui/kivy.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
+from kivy.uix.button import Button
+
+class liquidadorLaboral(App):
+
+    def biuld(self):
+        pass
