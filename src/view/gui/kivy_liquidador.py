ae90a8b01abac2bf1c93f44d756a9c640e86ef19
David Hernandez Mejia <davidmejia0951@gmail.com>
2025-04-03 10:43:44 -0500

    creacion del metodo calcular

--- src/view/gui/kivy_liquidador.py
+++ src/view/gui/kivy_liquidador.py
@@ -1,6 +1,7 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
+from model import liquidacion_total
 
 
 from kivy.app import App
@@ -9,112 +10,126 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
-from kivy.graphics import Color, Rectangle
+
 
 class liquidadorLaboral(App):
 
     def build(self):
-        contenedor = BoxLayout(orientation = "vertical")
-        contenedor_inputs = GridLayout(cols = 2)
+        self.contenedor = BoxLayout(orientation = "vertical")
+        self.contenedor_inputs = GridLayout(cols = 2)
 
-        titulo = Label(text = "Liquidador",
+        self.titulo = Label(text = "Liquidador",
                        font_size = 55,
                        height = 150,
                        size_hint = (1, None))
-        contenedor.add_widget(titulo)
+        self.contenedor.add_widget(self.titulo)
 
 
         label_fecha_inicio = Label(text = "Ingrese la fecha de inicio:",
                                    size_hint = (1 , None),
                                    height = 100,
                                    font_size = 30)
-        contenedor_inputs.add_widget(label_fecha_inicio)
+        self.contenedor_inputs.add_widget(label_fecha_inicio)
 
-        fecha_inicio = TextInput(size_hint = (1 , None),
+        self.fecha_inicio = TextInput(size_hint = (1 , None),
                                  height = 100,
                                  font_size = 25)
-        contenedor_inputs.add_widget(fecha_inicio)
+        self.contenedor_inputs.add_widget(self.fecha_inicio)
 
 
         label_fecha_fin = Label(text = "Ingrese la fecha de fin:",
                                 size_hint = (1 , None),
                                 height = 100,
                                 font_size = 30)
-        contenedor_inputs.add_widget(label_fecha_fin)
+        self.contenedor_inputs.add_widget(label_fecha_fin)
 
-        fecha_fin = TextInput(size_hint = (1 , None),
+        self.fecha_fin = TextInput(size_hint = (1 , None),
                               height = 100,
                               font_size = 25)
-        contenedor_inputs.add_widget(fecha_fin)
+        self.contenedor_inputs.add_widget(self.fecha_fin)
 
 
         label_salario_auxilio = Label(text = "Por favor ingrese su salario con auxilio:",
                                       size_hint = (1 , None),
                                       height = 100,
                                       font_size = 30)
-        contenedor_inputs.add_widget(label_salario_auxilio)
+        self.contenedor_inputs.add_widget(label_salario_auxilio)
 
-        salario_auxilio = TextInput(size_hint = (1 , None),
+        self.salario_auxilio = TextInput(size_hint = (1 , None),
                                     height = 100,
                                     font_size = 25)
-        contenedor_inputs.add_widget(salario_auxilio)
+        self.contenedor_inputs.add_widget(self.salario_auxilio)
 
 
         lable_salario_sin_auxilio = Label(text = "Ingrese su salario sin auxilio:",
                                           size_hint = (1 , None),
                                           height = 100,
                                           font_size = 30)
-        contenedor_inputs.add_widget(lable_salario_sin_auxilio)
+        self.contenedor_inputs.add_widget(lable_salario_sin_auxilio)
 
-        salario_sin_auxilio = TextInput(size_hint = (1 , None),
+        self.salario_sin_auxilio = TextInput(size_hint = (1 , None),
                                         height = 100,
                                         font_size = 25)
-        contenedor_inputs.add_widget(salario_sin_auxilio)
+        self.contenedor_inputs.add_widget(self.salario_sin_auxilio)
 
 
         label_dias_suspension = Label(text = "Ingrese sus dias de suspension:", 
                                       size_hint = (1 , None),
                                       height = 100,
                                       font_size = 30)
-        contenedor_inputs.add_widget(label_dias_suspension)
+        self.contenedor_inputs.add_widget(label_dias_suspension)
 
-        dias_suspension = TextInput(size_hint = (1 , None),
+        self.dias_suspension = TextInput(size_hint = (1 , None),
                                     height = 100,
                                     font_size = 25)
-        contenedor_inputs.add_widget(dias_suspension)
+        self.contenedor_inputs.add_widget(self.dias_suspension)
 
 
         label_dias_indemnizacion = Label(text = "Ingrese sus dias de indemnizacion:",
                                          size_hint = (1 , None),
                                          height = 100,
                                          font_size = 30)
-        contenedor_inputs.add_widget(label_dias_indemnizacion)
+        self.contenedor_inputs.add_widget(label_dias_indemnizacion)
 
-        dias_indemnizacion = TextInput(size_hint = (1 , None),
+        self.dias_indemnizacion = TextInput(size_hint = (1 , None),
                                        height = 100,
                                        font_size = 25)
-        contenedor_inputs.add_widget(dias_indemnizacion)
+        self.contenedor_inputs.add_widget(self.dias_indemnizacion)
 
 
         label_salario_variable = Label(text = "Ingrese su salario:",
                                        size_hint = (1 , None),
                                        height = 100,
                                        font_size = 30)
-        contenedor_inputs.add_widget(label_salario_variable)
+        self.contenedor_inputs.add_widget(label_salario_variable)
 
-        salario_variable = TextInput(size_hint = (1 , None),
+        self.salario_variable = TextInput(size_hint = (1 , None),
                                      height = 100,
                                      font_size = 25)
-        contenedor_inputs.add_widget(salario_variable)
+        self.contenedor_inputs.add_widget(self.salario_variable)
 
-        contenedor.add_widget(contenedor_inputs)
+        self.contenedor.add_widget(self.contenedor_inputs)
 
         boton_guardar_datos = Button(text = "Calcular", 
                                      font_size = 30,
                                      size_hint = (1, 0.15))
-        contenedor.add_widget(boton_guardar_datos)
+        boton_guardar_datos.bind(on_press = self.calcular())
+        self.contenedor.add_widget(boton_guardar_datos)
 
-        return contenedor
+        return self.contenedor
+
+    def calcular(self, sender):
+        fecha_inicio = self.fecha_inicio
+        fecha_fin = self.fecha_fin
+        salario_auxilio = self.salario_auxilio
+        salario_sin_auxilio = self.salario_sin_auxilio
+        dias_suspension = self.dias_suspension
+        dias_indemnizacion = self.dias_indemnizacion
+        salario_variable = self.salario_variable
+
+        liquidacion = liquidacion_total.LiquidacionEmpleado(salario_auxilio, salario_sin_auxilio, salario_variable, fecha_inicio, fecha_fin, dias_suspension, dias_indemnizacion)
+        
+
 
 
 if __name__ == "__main__":
