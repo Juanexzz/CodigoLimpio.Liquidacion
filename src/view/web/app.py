63925f74078b2abb8e4ad909e790ab5da654f056
SantiResFonn <159478006+SantiResFonn@users.noreply.github.com>
2025-05-26 08:59:05 -0500

    Add files via upload

--- src/view/web/app.py
+++ src/view/web/app.py
@@ -1,31 +1,12 @@
 from flask import Flask, render_template, request
-from src.controller.liquidaciones_controller import LiquidacionesController  # Importa el controlador
+import sys
+sys.path.append("src")
+from view.web import plano
+
 
 app = Flask(__name__)
 
-@app.route('/inicio')
-def Inicio():
-    return render_template("inicio.html")
-
-@app.route('/buscar', methods=['GET', 'POST'])
-def Buscar():
-    if request.method == 'POST':
-        id_busqueda = request.form['id_registro']
-        liquidacion = LiquidacionesController.buscar_por_id(id_busqueda)
-        return render_template("resultados_buscar.html", resultado=liquidacion)
-    return render_template("buscar.html")
-
-@app.route('/insertar')
-def Insertar():
-    return render_template("insertar.html")
-
-@app.route('/modificar')
-def Modificar():
-    return render_template("modificar.html")
-
-@app.route('/')
-def index():
-    return render_template("inicio.html")
+app.register_blueprint(plano.blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ src/view/web/plano.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, render_template, request
+import sys
+sys.path.append("src")
+from controller.liquidaciones_controller import LiquidacionesController 
+
+blueprint = Blueprint("vista_usuarios", __name__, "templates")
+
+@blueprint.route('/inicio')
+def Inicio():
+    return render_template("inicio.html")
+
+@blueprint.route('/buscar', methods=['GET', 'POST'])
+def Buscar():
+    if request.method == 'POST':
+        id_busqueda = request.form['id_registro']
+        liquidacion = LiquidacionesController.buscar_por_id(id_busqueda)
+        return render_template("resultados_buscar.html", resultado=liquidacion)
+    return render_template("buscar.html")
+
+@blueprint.route('/insertar')
+def Insertar():
+    return render_template("insertar.html")
+
+@blueprint.route('/modificar')
+def Modificar():
+    return render_template("modificar.html")
+
+@blueprint.route('/')
+def index():
+    return render_template("inicio.html")
+
+@blueprint.errorhandler(Exception)
+def manejar_error(err):
+    return "Ocurrio un error" + str(err)
